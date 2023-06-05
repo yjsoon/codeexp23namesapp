4cff1f3bff704e868fbea0659ad8b1f46b5a3c12
YJ Soon <yjsoon@tinkertanker.com>
2023-06-05 15:55:43 +0800

    Added more names and looped through them to display

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const names = [
+  "Aaran",
+  "Aaren",
+  "Aarez",
+  "Aarman",
+  "Aaron",
+  "Aaron-James",
+  "Aarron",
+  "Aaryan",
+  "Aaryn",
+  "Aayan",
+  "Aazaan",
+  "Abaan",
+  "Abbas",
+  "Abdallah",
+  "Abdalroof",
+  "Abdihakim",
+  "Abdirahman",
+  "Abdisalam",
+  "Abdul",
+  "Abdul-Aziz",
+  "Abdulbasir",
+  "Abdulkadir",
+  "Abdulkarem"
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  function displayNames() {
+    let nameJSX = [];
+    let name;
+    for (let i = 0; i < names.length; i++) {
+      nameJSX.push(<Text key={i}>{names[i]}</Text>);
+    }
+    return nameJSX;
+  }
+
+  return <View style={styles.container}>{displayNames()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
